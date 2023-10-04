9ad921c3d472bd796b403ba48ea9449f2b4e5d25
juan131 <jariza@vmware.com>
2023-10-04 13:20:33 +0200

    chore: add Makefile
    Signed-off-by: juan131 <jariza@vmware.com>

---
+++ vars.mk
@@ -0,0 +1,21 @@
+SHELL := /bin/bash
+GOPATH ?= $(shell go env GOPATH)
+PATH := $(GOPATH)/bin:$(PATH)
+BUILD_DIR := $(abspath ./out)
+
+BUILD_DATE := $(shell date -u '+%Y-%m-%d %I:%M:%S UTC' 2> /dev/null)
+GIT_HASH := $(shell git rev-parse HEAD 2> /dev/null)
+LDFLAGS="-X main.commit=$(GIT_HASH) -X 'main.buildDate=$(BUILD_DATE)' -s -w"
+
+DEBUG ?= 0
+
+ifeq ($(DEBUG),1)
+GO_TEST := @go test -v
+else
+GO_TEST := @go test
+endif
+
+GO_MOD := @go mod
+# Do not do goimport of the vendor dir
+go_files=$$(find $(1) -type f -name '*.go' -not -path "./vendor/*")
+fmtcheck = @if goimports -l $(go_files) | read var; then echo "goimports check failed for $(1):\n `goimports -d $(go_files)`"; exit 1; fi
